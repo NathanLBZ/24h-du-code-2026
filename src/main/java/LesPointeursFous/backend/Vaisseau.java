ab6392212f274236b59b9b0e257075b7071772a2
B0rno <enzo.desfaudais1@gmail.com>
2026-03-21 14:08:07 +0100

    Prototypage Vaisseau.java

---
+++ src/main/java/LesPointeursFous/backend/Vaisseau.java
@@ -0,0 +1,76 @@
+package LesPointeursFous.backend;
+
+public class Vaisseau {
+    private static Carte carte;
+    private int nbPV;
+    private int x;
+    private int y;
+
+    public Vaisseau(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.nbPV = 3;
+    }
+
+    public void Deplacer() {
+        // Return fetch Ilann
+    }
+
+    public void Recolter() {
+        // Return fetch Ilann
+
+    }
+
+    public void Deposer() {
+        // Return fetch Ilann
+
+    }
+
+    public void Attaquer(Planetes p) {
+       // Return fetch Ilann 
+    }
+
+    public void Attaquer(Vaisseau v) {
+       // Return fetch Ilann 
+    }
+
+    public void Reparer(Vaisseau v) {
+        // Return fetch Ilann
+    }
+
+    public void Reparer(Planetes p) {
+        // Return fetch Ilann
+    }
+
+    public boolean EstEnOrbite(Vaisseau v) {
+        if (carte.getCase(x, y) == Case.Planete) {
+            return true;
+        }
+        return false;
+    }
+
+    // Getters et Setters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getNbPV() {
+        return nbPV;
+    }
+
+    public void setNbPV(int nbPV) {
+        this.nbPV = nbPV;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
